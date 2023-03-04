1e9b32d59f9db07c069be60d55117762666d0495
emerlock <5019530+emerlock@users.noreply.github.com>
2023-03-04 12:32:12 -0500

    move ability to edit into RecipeEdit component. Set up the state for all form data. Start re-adding in the directions section. Wire up the inputs to their respective state objects. Change over config for the api to be authorized. Add comments explaining what is happening.

--- src/components/recipe/RecipeEdit.js
+++ src/components/recipe/RecipeEdit.js
@@ -1,35 +1,235 @@
-import React from "react";
-import RecipeForm from "./recipe_form/RecipeForm";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { MinusIcon, PlusIcon } from '@heroicons/react/solid';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-import { useDispatch, useSelector } from "react-redux";
-import { editRecipe } from "../../redux/actions/recipes";
+import { useDispatch, useSelector } from 'react-redux';
+import { editRecipe } from '../../redux/actions/recipes';
+import { getDetailRecipe } from '../../redux/actions/recipes';
 
 export default function RecipeEdit() {
-  
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  console.log(id)
+    // track state for what we need in the request
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [directions, setDirections] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
 
-  const { recipes } = useSelector((state) => state.recipes);
-  console.log("test:", recipes)
+    // track state for the items we can edit in the arrays
+    const [editDir, setEditDir] = useState('');
 
-  const recipe = recipes.filter((recipe) => recipe._id === id);
+    // dispatch for the api request
+    const dispatch = useDispatch();
 
-  console.log("is there a recipe?",recipe)
-  const handleFormSubmit = (formData) => {
-    dispatch(editRecipe(id, formData));
-  };
+    //params for the passed in id
+    const { id } = useParams();
 
-  return (
-    <div>
-      <RecipeForm
-        buttonLabel="Update"
-        handleFormSubmit={handleFormSubmit}
-        editMode={true}
-        recipe={recipe}
-      />
-    </div>
-  );
+    console.log(id);
+
+    const { detailRecipe } = useSelector((state) => state.recipes);
+    console.log('test:', detailRecipe);
+
+    const recipe = detailRecipe.response;
+
+    console.log('is there a recipe?', recipe);
+
+    //useEffect to preload all data after the dispatch.
+    //pass in empty array so useEffect only happens once
+    useEffect(() => {
+        dispatch(getDetailRecipe(id));
+        setTitle(detailRecipe.response.name);
+        setDesc(detailRecipe.response.description);
+        setDirections(detailRecipe.response.directions);
+        setIngredients(detailRecipe.response.ingredients);
+    }, []);
+
+    // submit the form
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+
+        formData.append('name', title);
+        formData.append('description', desc);
+        formData.append('directions', JSON.stringify(directions));
+        formData.append('ingredients', JSON.stringify(ingredients));
+        formData.append('notes', 'test notes');
+
+        dispatch(editRecipe(id, formData));
+    };
+
+    // add to the directions array
+    const onAddDirectionClick = () => {
+        setDirections((state) => [...state, editDir]);
+        setEditDir('');
+    };
+
+    return (
+        <>
+            <div>
+                <div className="md:grid md:grid-cols-3 md:gap-6">
+                    <div className="md:col-span-1">
+                        <div className="px-4 sm:px-0">
+                            <h3 className="p-5 text-lg font-medium leading-6 text-gray-900">
+                                Create your recipe and share it to the world!
+                            </h3>
+                            <p className="px-5 text-sm text-gray-600">
+                                "Cooking is like painting or writing a song.
+                                Just as there are only so many notes or colors,
+                                there are only so many flavors—it’s how you
+                                combine them that sets you apart."
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-5 md:mt-0 md:col-span-2">
+                        <form onSubmit={handleFormSubmit}>
+                            <div className="shadow sm:rounded-md sm:overflow-hidden">
+                                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                    <div>
+                                        <h1 className="text-lg leading-6 font-medium text-gray-900">
+                                            Title
+                                        </h1>
+                                        <input
+                                            type="text"
+                                            name="title"
+                                            id="title"
+                                            className="shadow-sm p-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 mt-1 block w-full border border-gray-300 rounded-md"
+                                            placeholder="Write a title for your recipe. Something catchy ..."
+                                            value={title}
+                                            onChange={(e) =>
+                                                setTitle(e.target.value)
+                                            }
+                                        />
+                                    </div>
+                                    <div>
+                                        <h1 className="text-lg leading-6 font-medium text-gray-900">
+                                            Description
+                                        </h1>
+                                        <div className="mt-1">
+                                            <textarea
+                                                id="desc"
+                                                name="desc"
+                                                rows={3}
+                                                className="shadow-sm p-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                                placeholder="Write a short description..."
+                                                value={desc}
+                                                onChange={(e) =>
+                                                    setDesc(e.target.value)
+                                                }
+                                            />
+                                        </div>
+                                        <p className="mt-2 text-sm text-gray-500">
+                                            Write a short and precise
+                                            description abour your recipe.
+                                        </p>
+                                    </div>
+                                    <div className="space-y-6">
+                                        <div>
+                                            <h1 className="text-lg leading-6 font-medium text-gray-900">
+                                                Directions
+                                            </h1>
+                                            <p className="mt-1 text-sm text-gray-500">
+                                                Fill in the directions to create
+                                                your new recipe.
+                                            </p>
+                                        </div>
+
+                                        <div className="space-y-2">
+                                            <div className="space-y-1">
+                                                <div className="flex">
+                                                    <div className="flex-grow">
+                                                        <textarea
+                                                            id="description"
+                                                            name="description"
+                                                            rows={1}
+                                                            className="block w-full shadow-sm p-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm border border-gray-300 rounded-md"
+                                                            placeholder="Add egg and meat until egg mixture is combined..."
+                                                            value={editDir}
+                                                            onChange={(
+                                                                event
+                                                            ) => {
+                                                                setEditDir(
+                                                                    event.target
+                                                                        .value
+                                                                );
+                                                            }}
+                                                        />
+                                                    </div>
+                                                    <div className="ml-3">
+                                                        <button
+                                                            type="button"
+                                                            className="bg-white inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                                                            onClick={
+                                                                onAddDirectionClick
+                                                            }
+                                                        >
+                                                            <PlusIcon
+                                                                className="-ml-2 mr-1 h-5 w-5 text-gray-400"
+                                                                aria-hidden="true"
+                                                            />
+                                                            <span>Add</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="border-b border-gray-200">
+                                                <ul className="divide-y divide-gray-200">
+                                                    {directions.map(
+                                                        (direction, idx) => (
+                                                            <li
+                                                                key={direction}
+                                                                className="py-4 flex"
+                                                            >
+                                                                <div className="ml-3 flex flex-grow justify-between">
+                                                                    <div>
+                                                                        <span className="text-base font-medium text-gray-900">
+                                                                            {idx +
+                                                                                1}
+                                                                            ){' '}
+                                                                        </span>
+                                                                        <span className="text-base font-medium text-gray-900">
+                                                                            {
+                                                                                direction
+                                                                            }
+                                                                        </span>
+                                                                    </div>
+
+                                                                    <div className="">
+                                                                        <button
+                                                                            type="button"
+                                                                            className="bg-white inline-flex items-center px-2 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                                                                        >
+                                                                            <MinusIcon
+                                                                                className="-ml-2 mr-1 h-5 w-5 text-gray-400"
+                                                                                aria-hidden="true"
+                                                                            />
+                                                                            <span>
+                                                                                Remove
+                                                                            </span>
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                        )
+                                                    )}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button
+                                        type="submit"
+                                        className="w-full bg-teal-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-cyan-500"
+                                    >
+                                        Edit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
--- src/redux/actions/recipes.js
+++ src/redux/actions/recipes.js
@@ -1,121 +1,123 @@
 import {
-  RECIPE_LOADING,
-  GET_RECIPES,
-  GET_DETAIL_RECIPE,
-  GET_ERRORS,
-  CREATE_RECIPE,
-  EDIT_RECIPE,
-  DELETE_RECIPE,
-  CLEAR_RECIPE,
-} from "./types";
-import axiosInstance from "../../utils/axios";
-import { tokenConfig } from "./auth";
+    RECIPE_LOADING,
+    GET_RECIPES,
+    GET_DETAIL_RECIPE,
+    GET_ERRORS,
+    CREATE_RECIPE,
+    EDIT_RECIPE,
+    DELETE_RECIPE,
+    CLEAR_RECIPE
+} from './types';
+import axiosInstance from '../../utils/axios';
+import { tokenConfig } from './auth';
 
 const config = {
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": localStorage.getItem("recipe") ? localStorage.getItem("recipe").slice(1, localStorage.getItem("recipe").length -1): undefined // this is a hack, but for some reason extra quotes are making this unauthorized
-  },
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('recipe')
+            ? localStorage
+                  .getItem('recipe')
+                  .slice(1, localStorage.getItem('recipe').length - 1)
+            : undefined // this is a hack, but for some reason extra quotes are making this unauthorized
+    }
 };
 
 export const getRecipes = () => (dispatch) => {
-  dispatch({ type: RECIPE_LOADING });
-  axiosInstance
-    .get("/recipes", config)
-    .then((res) => {
-      dispatch({
-        type: GET_RECIPES,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response,
-      });
-    });
+    dispatch({ type: RECIPE_LOADING });
+    axiosInstance
+        .get('/recipes', config)
+        .then((res) => {
+            dispatch({
+                type: GET_RECIPES,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response
+            });
+        });
 };
 
 export const getDetailRecipe = (id) => (dispatch, getState) => {
-  dispatch({ type: RECIPE_LOADING });
+    dispatch({ type: RECIPE_LOADING });
 
-  axiosInstance
-    .get(`/recipes/${id}/`, config)
-    .then((res) => {
-      dispatch({
-        type: GET_DETAIL_RECIPE,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response,
-      });
-    });
+    axiosInstance
+        .get(`/recipes/${id}/`, config)
+        .then((res) => {
+            dispatch({
+                type: GET_DETAIL_RECIPE,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response
+            });
+        });
 };
 
 export const createRecipe = (formData) => (dispatch, getState) => {
-  dispatch({ type: RECIPE_LOADING });
+    dispatch({ type: RECIPE_LOADING });
 
-  axiosInstance
-    .post("/recipes/create/", formData, tokenConfig(getState))
-    .then((res) => {
-      dispatch({
-        type: CREATE_RECIPE,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response,
-      });
-    });
+    axiosInstance
+        .post('/recipes/create/', formData, tokenConfig(getState))
+        .then((res) => {
+            dispatch({
+                type: CREATE_RECIPE,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response
+            });
+        });
 };
 
 export const editRecipe = (id, formData) => (dispatch, getState) => {
-  dispatch({ type: RECIPE_LOADING });
+    dispatch({ type: RECIPE_LOADING });
 
-  axiosInstance
-    .patch(`/recipes/${id}/`, formData, tokenConfig(getState))
-    .then((res) => {
-      dispatch({
-        type: EDIT_RECIPE,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response,
-      });
-    });
+    axiosInstance
+        .patch(`/recipes/${id}/`, formData, config)
+        .then((res) => {
+            dispatch({
+                type: EDIT_RECIPE,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response
+            });
+        });
 };
 
 export const deleteRecipe = (id) => (dispatch, getState) => {
-  dispatch({ type: RECIPE_LOADING });
+    dispatch({ type: RECIPE_LOADING });
 
-  axiosInstance
-    .delete(`/recipes/${id}/`, tokenConfig(getState))
-    .then((res) => {
-      dispatch({
-        type: DELETE_RECIPE,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response,
-      });
-    });
+    axiosInstance
+        .delete(`/recipes/${id}/`, tokenConfig(getState))
+        .then((res) => {
+            dispatch({
+                type: DELETE_RECIPE,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response
+            });
+        });
 };
 
-export const clearRecipes =
-  () =>
-  (dispatch, getState) => {
+export const clearRecipes = () => (dispatch, getState) => {
     dispatch({
-      type: CLEAR_RECIPE
-    })
+        type: CLEAR_RECIPE
+    });
 };
