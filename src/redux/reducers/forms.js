1a564e00102167445bba44c13e69e57f3aea6feb
emerlock <5019530+emerlock@users.noreply.github.com>
2023-03-04 19:48:00 -0500

    remove time comp, category comp, picture comp, and remove commented out code, and a console log

--- src/redux/reducers/forms.js
+++ src/redux/reducers/forms.js
@@ -1,48 +1,34 @@
 /* eslint-disable import/no-anonymous-default-export */
 import {
-  //ADD_CATEGORY,
-  ADD_COOKTIME,
-  ADD_INGREDIENTS,
-  //ADD_PICTURE,
-  ADD_PROCEDURES,
-} from "../actions/types";
+    ADD_COOKTIME,
+    ADD_INGREDIENTS,
+    ADD_PROCEDURES
+} from '../actions/types';
 
 const initialState = {
-  ingredients: [],
-  procedures: [],
-  cook_time: "",
-  category: "",
-  picture: [],
+    ingredients: [],
+    procedures: [],
+    picture: []
 };
 
 export default function (state = initialState, action) {
-  switch (action.type) {
-    case ADD_INGREDIENTS:
-      return {
-        ...state,
-        ingredients: action.payload,
-      };
-    case ADD_PROCEDURES:
-      return {
-        ...state,
-        procedures: action.payload,
-      };
-    case ADD_COOKTIME:
-      return {
-        ...state,
-        cook_time: action.payload,
-      };
-    {/*case ADD_CATEGORY:
-      return {
-        ...state,
-        category: action.payload,
-      };
-    case ADD_PICTURE:
-      return {
-        ...state,
-        picture: action.payload,
-      };*/}
-    default:
-    return state;
-  }
+    switch (action.type) {
+        case ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: action.payload
+            };
+        case ADD_PROCEDURES:
+            return {
+                ...state,
+                procedures: action.payload
+            };
+        case ADD_COOKTIME:
+            return {
+                ...state,
+                cook_time: action.payload
+            };
+        default:
+            return state;
+    }
 }
